4836aaca9b5a23a786be0ccc6af15a388c8d5958
matthias <matthias.ooye@euri.com>
2019-08-23 08:48:39 +0200

    lintingfixes

--- Kudobox-frontend/src/app/components/send/send.component.spec.ts
+++ Kudobox-frontend/src/app/components/send/send.component.spec.ts
@@ -1,25 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SendComponent } from './send.component';
 
 describe('SendComponent', () => {
-  let component: SendComponent;
-  let fixture: ComponentFixture<SendComponent>;
+    let component: SendComponent;
+    let fixture: ComponentFixture<SendComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SendComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SendComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SendComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SendComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- Kudobox-frontend/src/app/shared/kudo.service.ts
+++ Kudobox-frontend/src/app/shared/kudo.service.ts
@@ -2,32 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class KudoService {
-  private _imageDataURL: string;
-  private _user: string;
+    private _imageDataURL: string;
+    private _user: string;
 
-  constructor(private http:HttpClient) { }
+    constructor(private http: HttpClient) {}
 
-  set imageDataURL(imageDataURL: string) {
-    this._imageDataURL = imageDataURL;
-  }
+    set imageDataURL(imageDataURL: string) {
+        this._imageDataURL = imageDataURL;
+    }
 
-  get imageDataURL(): string {
-    return this._imageDataURL;
-  }
+    get imageDataURL(): string {
+        return this._imageDataURL;
+    }
 
-  set user(user: string) {
-    this._user = user;
-  }
+    set user(user: string) {
+        this._user = user;
+    }
 
-  get user(): string {
-    return this._user;
-  }
+    get user(): string {
+        return this._user;
+    }
 
-  sendKudo(kudo) {
-    let body = JSON.stringify(kudo);
-    return this.http.post('/api/sendKudo', body);
-  }
+    sendKudo(kudo) {
+        const body = JSON.stringify(kudo);
+        return this.http.post('/api/sendKudo', body);
+    }
 }
