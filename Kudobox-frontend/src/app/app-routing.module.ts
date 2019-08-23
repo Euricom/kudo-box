ec77dd85f75145e9f2d34575c9dba65ffc2a4f78
Silke Venneman <silke.venneman@euri.com>
2019-08-23 07:41:33 +0200

    fix: merge conflicts

--- Kudobox-frontend/src/app/app-routing.module.ts
+++ Kudobox-frontend/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateComponent } from './components/create/create.component';
 import { NewKudoComponent } from './components/newKudo/newKudo.component';
 import { MyKudoComponent } from './components/my-kudo/my-kudo.component';
+import { SendComponent } from './components/send/send.component';
 
 const routes: Routes = [
     { path: 'kudo/create/:id', component: CreateComponent },
+    { path: 'kudo/send', component: SendComponent },
     { path: 'kudo', component: NewKudoComponent },
     { path: 'myKudo', component: MyKudoComponent },
     { path: '', redirectTo: 'kudo', pathMatch: 'full' },
