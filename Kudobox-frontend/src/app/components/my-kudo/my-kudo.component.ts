781af7f361aaa9cdbf6652f73c4b08fed676a25c
Silke Venneman <silke.venneman@euri.com>
2019-08-26 12:47:49 +0200

    feat: all kudos page

--- Kudobox-frontend/src/app/components/my-kudo/my-kudo.component.ts
+++ Kudobox-frontend/src/app/components/my-kudo/my-kudo.component.ts
@@ -8,6 +8,8 @@ import { kudoImages } from '../../data/kudoImages';
 })
 export class MyKudoComponent implements OnInit {
     public kudos: Array<object>;
+    public linkToShare: string = 'https://google.com';
+    public image = 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/237864';
 
     ngOnInit() {
         this.kudos = kudoImages;
