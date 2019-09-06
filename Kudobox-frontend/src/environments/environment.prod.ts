4f536ceaf57447662bfb3a47607433997da68004
Matthias <matthias.ooye@euri.com>
2019-09-06 14:21:45 +0200

    test config build

--- Kudobox-frontend/src/environments/environment.prod.ts
+++ Kudobox-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,16 @@
 import * as Logger from 'js-logger';
-import config from './settings.json';
 
 export const environment = {
     logLevel: Logger.DEBUG,
     production: true,
-    apiUrl: config.apiUrl,
-    oidc: config.oidc,
+    apiUrl: '#{apiUrl}#',
+    oidc: {
+        authority: 'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/v2.0/',
+        scope: 'openid profile api://kudobox.euri.com/api',
+        redirect_uri: '#{redirect_uri}#',
+        response_type: 'id_token token',
+        client_id: 'de411acd-f5d7-4040-8da6-3d3adce56901',
+        loadUserInfo: false,
+        post_logout_redirect_uri: '#{post_logout_redirect_uri}#',
+    },
 };
