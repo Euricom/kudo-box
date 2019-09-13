8bcb7eb906e1f78dbdc09e5319f525fa9fb7050c
Silke Venneman <silke.venneman@euri.com>
2019-09-13 07:37:40 +0200

    make it working locally

--- Kudobox-frontend/src/server/index.js
+++ Kudobox-frontend/src/server/index.js
@@ -7,16 +7,33 @@ const options = {
     index: 'index.html',
     redirect: true,
 };
-server.use(express.static('/home/site/wwwroot', options));
+// server.use(express.static('/home/site/wwwroot', options));
+const staticPath = path.join(__dirname, '..');
+console.log('PATH', staticPath);
+server.use(express.static(staticPath, options));
 const allowedExt = ['.js', '.ico', '.css', '.png', '.jpg', '.woff2', '.woff', '.ttf', '.svg'];
 
 server.get('*', (req, res) => {
+    console.log('server.get');
     if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
         res.sendFile(path.resolve(req.url));
     } else {
         fs.readFile(path.resolve('index.html'), 'utf8', (err, html) => {
-            const responseHtml = html.replace(/#IMAGE#/g, 'https://kudobox-dev.azurewebsites.net/assets/You_Rock.jpg');
-            res.send(responseHtml);
+            if (req.path.indexOf('share-kudo')) {
+                const pathArray = req.path.split('/');
+                console.log('ID', pathArray[pathArray.length - 1]);
+                const responseHtml = html.replace(
+                    /#IMAGE#/g,
+                    `https://kudobox-api-dev.azurewebsites.net/api/kudo/${pathArray[pathArray.length - 1]}/getImage`,
+                );
+                res.send(responseHtml);
+                /* const responseHtml = html.replace(
+                    /#IMAGE#/g,
+                    'https://kudobox-dev.azurewebsites.net/assets/You_Rock.jpg',
+                ); */
+            } else {
+                res.send(html);
+            }
         });
     }
 });
