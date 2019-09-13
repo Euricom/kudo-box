a9a062ad4455a4394ff4bebaddc1fd3b723a0031
Matthias <matthias.ooye@euri.com>
2019-09-13 14:19:46 +0200

    url added to sharebuttons

--- Kudobox-frontend/src/app/share-kudo/share-kudo.component.ts
+++ Kudobox-frontend/src/app/share-kudo/share-kudo.component.ts
@@ -4,21 +4,22 @@ import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-share-kudo',
-  templateUrl: './share-kudo.component.html',
-  styleUrls: ['./share-kudo.component.scss']
+    selector: 'app-share-kudo',
+    templateUrl: './share-kudo.component.html',
+    styleUrls: ['./share-kudo.component.scss'],
 })
 export class ShareKudoComponent implements OnInit {
-  public baseLocation = window.location.origin;
-  public kudoId;
-  public baseImageLocation;
-  constructor(private meta: Meta,private route: ActivatedRoute) {
+    public baseLocation = window.location.origin;
+    public publicKudoUrl;
+    public kudoId;
+    public baseImageLocation;
+    constructor(private meta: Meta, private route: ActivatedRoute) {
+        this.route.params.subscribe(routeParams => {
+            this.kudoId = routeParams.id;
+        });
+        this.publicKudoUrl = `${this.baseLocation}/public-kudo/${this.kudoId}`;
+        this.baseImageLocation = `${environment.apiUrl}/api/kudo/${this.kudoId}/getImage`;
+    }
 
-    this.route.params.subscribe(routeParams => this.kudoId =routeParams.id);
-    this.baseImageLocation = `${environment.apiUrl}/api/kudo/${this.kudoId}/getImage`;
-  }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
