afa30062c544d1c3eb4211cb8d9cedc3469d8498
Matthias <matthias.ooye@euri.com>
2019-09-19 09:50:06 +0200

    remove compress

--- Kudobox-frontend/src/server/index.js
+++ Kudobox-frontend/src/server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const compression = require('compression');
+// const compression = require('compression');
 
 const server = express();
-server.use(compression);
+// server.use(compression);
 
 const options = {
     index: 'index.html',
