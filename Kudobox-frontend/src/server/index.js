d06ddf03ee7f0addf907fb9571696d83d544b7b4
Matthias <matthias.ooye@euri.com>
2019-09-19 12:22:53 +0200

    add compression to frontend

--- Kudobox-frontend/src/server/index.js
+++ Kudobox-frontend/src/server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const compression = require('compression');
+const compression = require('compression');
 
 const server = express();
-// server.use(compression);
+server.use(
+    compression({
+        filter(req, res) {
+            return true;
+        },
+    }),
+);
 
 const options = {
     index: 'index.html',
@@ -16,19 +22,25 @@ server.use(express.static(staticPath, options));
 const allowedExt = ['.js', '.ico', '.css', '.png', '.jpg', '.woff2', '.woff', '.ttf', '.svg'];
 
 server.get('*', (req, res) => {
+    console.log('getting:', req.url);
     if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+        console.log('wut 1');
+
         res.sendFile(path.resolve(req.url));
     } else {
+        console.log('wut 2');
         fs.readFile(path.resolve('index.html'), 'utf8', (err, html) => {
             if (req.path.indexOf('share-kudo') !== -1 || req.path.indexOf('public-kudo') !== -1) {
                 const pathArray = req.path.split('/');
                 let responseHtml = html.replace(
                     /#IMAGE#/g,
                     `https://kudobox-api-dev.azurewebsites.net/api/kudo/${pathArray[pathArray.length - 1]}/getImage`,
+                    // `http://localhost:4200/api/kudo/${pathArray[pathArray.length - 1]}/getImage`,
                 );
                 responseHtml = responseHtml.replace(
                     /#URL#/g,
                     `https://kudobox-dev.azurewebsites.net/public-kudo/${pathArray[pathArray.length - 1]}`,
+                    // `http://localhost:4200/public-kudo/${pathArray[pathArray.length - 1]}`,
                 );
                 res.send(responseHtml);
                 /* const responseHtml = html.replace(
@@ -41,4 +53,6 @@ server.get('*', (req, res) => {
         });
     }
 });
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 4200, () => {
+    console.log('port', process.env.PORT || 4200);
+});
