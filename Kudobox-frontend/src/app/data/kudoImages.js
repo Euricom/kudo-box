d44c62e8d951a0be831a7ec61da8af5356488889
Silke Venneman <silke.venneman@euri.com>
2019-09-20 12:02:29 +0200

    download 1 image

--- Kudobox-frontend/src/app/data/kudoImages.js
+++ Kudobox-frontend/src/app/data/kudoImages.js
@@ -2,7 +2,10 @@ export const kudoImages = [
     {
         id: 1,
         url: '/assets/Well_Done.jpg',
+        title: 'Well Done'
     },
-    { id: 2, url: '/assets/Bravocado.jpg' },
-    { id: 3, url: '/assets/You_Rock.jpg' },
+    { id: 2, url: '/assets/Bravocado.jpg',
+title: 'Bravocade' },
+    { id: 3, url: '/assets/You_Rock.jpg',
+title:'You Rock' },
 ];
--- Kudobox-frontend/src/app/my-kudos/my-kudos.component.ts
+++ Kudobox-frontend/src/app/my-kudos/my-kudos.component.ts
@@ -47,22 +47,65 @@ export class MyKudosComponent implements OnInit {
             }
             return false;
         });
-        return image[0].url;
+        return {
+          url: image[0].url,
+          title: image[0].title};
     }
 
     shareImage(id, kudoId) {
         const divId = `capture-${id}`;
         const node = document.getElementById(divId);
         const serv = this._kudoService;
-        htmlToImage
+        this.htmlToPng(node).then(dataUrl => {
+            serv.saveKudoImage(kudoId, { data: dataUrl }).subscribe(() =>
+                this.router.navigateByUrl(`/share-kudo/${kudoId}`),
+            );
+        });
+    }
+
+    htmlToPng(node) {
+        return htmlToImage
             .toPng(node)
             .then(dataUrl => {
-                serv.saveKudoImage(kudoId, { data: dataUrl }).subscribe(() =>
-                    this.router.navigateByUrl(`/share-kudo/${kudoId}`),
-                );
+                return dataUrl;
             })
             .catch(function(error) {
                 console.error('oops, something went wrong!', error);
             });
+    }
+
+    convertDataURIToBinary(dataUrl) {
+        var base64Index = dataUrl.indexOf(';base64,') + ';base64,'.length;
+        var base64 = dataUrl.substring(base64Index);
+        var raw = window.atob(base64);
+        var rawLength = raw.length;
+        var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+        for (let i = 0; i < rawLength; i++) {
+            array[i] = raw.charCodeAt(i);
+        }
+        return array;
+    }
+
+    downloadImage(id) {
+        const divId = `capture-${id}`;
+        const node = document.getElementById(divId);
+        const serv = this._kudoService;
+        this.htmlToPng(node).then(dataUrl => {
+            this.downloadImages(dataUrl);
+        });
+    }
+
+    downloadImages(image) {
+        const binaryData = this.convertDataURIToBinary(image);
+        var url = window.URL.createObjectURL(new Blob([binaryData], { type: 'image/png' }));
+        var a = document.createElement('a');
+        document.body.appendChild(a);
+        a.setAttribute('style', 'display: none');
+        a.href = url;
+        a.download = 'test';
+        a.click();
+        window.URL.revokeObjectURL(url);
+        a.remove(); // remove the element
     }
 }
