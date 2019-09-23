dae7af3f0d5333323190a4b5bccd23f447b6fef9
Matthias <matthias.ooye@euri.com>
2019-09-23 12:33:52 +0200

    config changes

--- Kudobox-frontend/src/environments/environment.prod.ts
+++ Kudobox-frontend/src/environments/environment.prod.ts
@@ -1,39 +1,38 @@
 import * as Logger from 'js-logger';
+import config from './settings.json';
 
 export const environment = {
     logLevel: Logger.DEBUG,
     production: true,
-    apiUrl: 'https://kudobox-api-dev.azurewebsites.net',
+    apiUrl: config.apiUrl,
     socketIo: {
-        socketIoUrl: 'https://kudobox-api-dev.azurewebsites.net',
+        socketIoUrl: config.socketIo.socketIoUrl,
     },
     oidc: {
         authority: 'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/v2.0/',
         scope: 'openid profile api://kudobox.euri.com/api',
-        redirect_uri: 'https://kudobox-dev.azurewebsites.net/auth',
+        redirect_uri: config.oidc.redirect_uri,
         response_type: 'id_token token',
         client_id: 'de411acd-f5d7-4040-8da6-3d3adce56901',
         loadUserInfo: false,
-        post_logout_redirect_uri: 'https://kudobox-dev.azurewebsites.net',
+        post_logout_redirect_uri: config.oidc.post_logout_redirect_uri,
     },
 };
-
-// import * as Logger from 'js-logger';
 
 // export const environment = {
 //     logLevel: Logger.DEBUG,
 //     production: true,
-//     apiUrl: '#{apiUrl}#',
+//     apiUrl: 'https://kudobox-api-dev.azurewebsites.net',
 //     socketIo: {
-//         socketIoUrl: '#{apiUrl}#',
+//         socketIoUrl: 'https://kudobox-api-dev.azurewebsites.net',
 //     },
 //     oidc: {
 //         authority: 'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/v2.0/',
 //         scope: 'openid profile api://kudobox.euri.com/api',
-//         redirect_uri: '#{redirect_uri}#',
+//         redirect_uri: 'https://kudobox-dev.azurewebsites.net/auth',
 //         response_type: 'id_token token',
 //         client_id: 'de411acd-f5d7-4040-8da6-3d3adce56901',
 //         loadUserInfo: false,
-//         post_logout_redirect_uri: '#{post_logout_redirect_uri}#',
+//         post_logout_redirect_uri: 'https://kudobox-dev.azurewebsites.net',
 //     },
 // };
