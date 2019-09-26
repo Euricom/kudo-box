2677bd77efe54a01f72e05a8f49d57764e359ecf
Matthias <matthias.ooye@euri.com>
2019-09-26 09:35:17 +0200

    change config

--- Kudobox-frontend/src/environments/environment.ts
+++ Kudobox-frontend/src/environments/environment.ts
@@ -1,20 +1,19 @@
 import * as Logger from 'js-logger';
-import config from './settings.json';
 
 export const environment = {
     logLevel: Logger.DEBUG,
     production: false,
-    apiUrl: 'https://kudobox-api-dev.azurewebsites.net',
+    apiUrl: 'http://localhost:4001',
     socketIo: {
-        socketIoUrl: config.socketIo.socketIoUrl,
+        socketIoUrl: 'http://localhost:4001',
     },
     oidc: {
         authority: 'https://login.microsoftonline.com/0b53d2c1-bc55-4ab3-a161-927d289257f2/v2.0/',
         scope: 'openid profile api://kudobox.euri.com/api',
-        redirect_uri: config.oidc.redirect_uri,
+        redirect_uri: 'http://localhost:4200',
         response_type: 'id_token token',
         client_id: 'de411acd-f5d7-4040-8da6-3d3adce56901',
         loadUserInfo: false,
-        post_logout_redirect_uri: config.oidc.post_logout_redirect_uri,
+        post_logout_redirect_uri: 'http://localhost:4200',
     },
 };
