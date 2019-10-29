3ae70dce736b758e193a9d089676a0f3a42d908b
Matthias <matthias.ooye@euri.com>
2019-10-29 09:54:19 +0100

    next fix

--- Kudobox-backend/src/index.js
+++ Kudobox-backend/src/index.js
@@ -178,9 +178,11 @@ async function screenshotDOMElement(kudo, baseUrl, opts = {}) {
       "--proxy-server='direct://'",
       "--proxy-bypass-list=*"
     ]
-    
   });
   const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+  );
   await page.setContent(htmlstring);
 
   const padding = "padding" in opts ? opts.padding : 0;
